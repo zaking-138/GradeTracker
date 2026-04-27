16846d1c8322ebac5323d0e532033181dfea1f9e
adamv <adamvartan@hotmail.com>
2026-04-26 23:47:07 -0700

    worked on test file

---
+++ src/test/java/AdamVartanTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class AdamVartanTests {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
